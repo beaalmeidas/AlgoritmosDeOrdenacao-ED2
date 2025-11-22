d9fed70eb254dd15fe0c2fe0e7ffa3339fe10e90
Beatriz Almeida <beatrizalmeidabss@gmail.com>
2025-11-22 18:22:46 -0300

    chore: organizando o código dos utilitários

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -9,68 +9,63 @@ import utils.Utils;
 public class MainMenu {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int opcao;
+        try (Scanner scan = new Scanner(System.in)) {
+            int opcao;
 
-        BubbleSort bubble = new BubbleSort();
-        InsertionSort insertion = new InsertionSort();
-        MergeSort merge = new MergeSort();
-        QuickSort quick = new QuickSort();
-        SelectionSort selection = new SelectionSort();
+            BubbleSort bubble = new BubbleSort();
+            InsertionSort insertion = new InsertionSort();
+            MergeSort merge = new MergeSort();
+            QuickSort quick = new QuickSort();
+            SelectionSort selection = new SelectionSort();
 
-        do {
-            System.out.println("\n====== MENU ======");
-            System.out.println("1. Bubble Sort");
-            System.out.println("2. Insertion Sort");
-            System.out.println("3. Merge Sort");
-            System.out.println("4. Quick Sort");
-            System.out.println("5. Selection Sort");
-            System.out.println("6. Encerrar");
-            System.out.print("Escolha uma opção: ");
-            opcao = scan.nextInt();
-            scan.nextLine();
-            System.out.print("\n");
+            do {
+                System.out.println("\n====== MENU ======");
+                System.out.println("1. Bubble Sort");
+                System.out.println("2. Insertion Sort");
+                System.out.println("3. Merge Sort");
+                System.out.println("4. Quick Sort");
+                System.out.println("5. Selection Sort");
+                System.out.println("6. Encerrar");
+                System.out.print("Escolha uma opção: ");
+                opcao = scan.nextInt();
+                scan.nextLine();
+                System.out.print("\n");
 
-            int[] vetor = Utils.gerarVetor(10, 51);
+                int[] vetor = Utils.gerarVetor(10, 51);
 
-            switch (opcao) {
-                case 1: 
-                    System.out.println("Você escolheu: Bubble Sort");
-                    bubble.ordenarVetor(vetor);
-                    break;
+                switch (opcao) {
+                    case 1 -> {
+                        System.out.println("Você escolheu: Bubble Sort");
+                        bubble.ordenarVetor(vetor);
+                    }
 
-                case 2: 
-                    System.out.println("Você escolheu: Insertion Sort");
-                    insertion.ordenarVetor(vetor);
-                    break;
-                    
-                    case 3: 
-                    System.out.println("Você escolheu: Merge Sort");
-                    merge.ordenarVetor(vetor);
-                    break;
-                    
-                    case 4: 
-                    System.out.println("Você escolheu: Quick Sort");
-                    quick.ordenarVetor(vetor);
-                    break;
+                    case 2 -> {
+                        System.out.println("Você escolheu: Insertion Sort");
+                        insertion.ordenarVetor(vetor);
+                    }
 
-                case 5:
-                    System.out.println("Você escolheu: Selection Sort");
-                    selection.ordenarVetor(vetor);
-                    break;
+                    case 3 -> {
+                        System.out.println("Você escolheu: Merge Sort");
+                        merge.ordenarVetor(vetor);
+                    }
 
-                case 6:
-                    System.out.println("Saindo...");
-                    break;
+                    case 4 -> {
+                        System.out.println("Você escolheu: Quick Sort");
+                        quick.ordenarVetor(vetor);
+                    }
 
-                default:
-                    System.out.println("Opção inválida! Tente novamente.");
-                    break;
-            }
+                    case 5 -> {
+                        System.out.println("Você escolheu: Selection Sort");
+                        selection.ordenarVetor(vetor);
+                    }
 
-            System.out.println();
-        } while (opcao != 6);
+                    case 6 -> System.out.println("Saindo...");
 
-        scan.close();
+                    default -> System.out.println("Opção inválida! Tente novamente.");
+                }
+
+                System.out.println();
+            } while (opcao != 6);
+        }
     }
 }
--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,58 +1,38 @@
-
 package utils;
 
 import java.util.Random;
 
 /**
- * 
  * @author Hilario Tomaz Alves de Oliveira
- *
- */
+*/
 
 public class Utils {
+	private Utils() {
+	}
 
-  private Utils() {
+	public static void imprimirVetor(int[] vetor) {
+		System.out.print( "  Vetor = {" );
 
-  }
-  
-  // M�todos
-  
-  public static void imprimirVetor(int[] vetor) {
-	
-	System.out.print( "  Vetor = {" );
-	  
-    for(int i = 0; i < vetor.length; i++) {
-    	
-      System.out.print( vetor[i] );
-      
-      if( i < ( vetor.length - 1 ) ) {
-    	  
-        System.out.print( ", " );
-    	  
-      }
-      
-    }
-    
-    System.out.print( "}" );
-    
-  }
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.print( vetor[i] );
 
-  public static int[] gerarVetor(int tamanhoVetor, int limiteSuperior) {
-	
-	Random random = new Random();
-	
-	int[] vetor = new int[tamanhoVetor];
-	
-    for(int i = 0; i < tamanhoVetor; i++) {
-	    	
-	  int numero = random.nextInt( limiteSuperior );
-	 
-	  vetor[i] = numero;
-	  
-    } // FOR VETOR
-	    
-    return vetor;
-    
-  }
+			if( i < ( vetor.length - 1 ) ) {
+				System.out.print( ", " );
+			}
+		}
 
+		System.out.print( "}" );
+	}
+
+	public static int[] gerarVetor(int tamanhoVetor, int limiteSuperior) {
+		Random random = new Random();
+		int[] vetor = new int[tamanhoVetor];
+
+		for(int i = 0; i < tamanhoVetor; i++) {
+			int numero = random.nextInt( limiteSuperior );
+			vetor[i] = numero;
+		}
+
+		return vetor;
+	}
 }
